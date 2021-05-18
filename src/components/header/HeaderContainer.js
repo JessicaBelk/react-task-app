50c77fc3cea1aeefacf099f3ffd2ad2e6119dff1
JessicaBelk <45734876+JessicaBelk@users.noreply.github.com>
2021-05-18 09:05:55 -0400

    Update HeaderContainer.js
    My goal is to make a login that routes home

--- src/components/header/HeaderContainer.js
+++ src/components/header/HeaderContainer.js
@@ -1,13 +1,24 @@
-import React from "react";
-import "./header.css";
-
-const HeaderContainer = () => {
-
+import React from 'react';
+import {NavLink, withRouter} from 'react-router-dom'
+import HomeContainer from '../../Components/home/HomeContainer'
+import './header.css';
+const HeaderContainer = ({history,isLogged}) =>{
+    const handleClick=() =>{
+        history.push('/')
+        isLogged(false)
+    }
     return(
-        <div className="Header">
-            This is the Header Component
-        </div>
+        <nav>
+            <div className='div-header'>
+                <div className='div-svg' onClick={() => history.push('/')}>
+                    <Logo/>
+                </div>
+                <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
+                    <button className='button-header' onClick={handleClick}>Log out</button>
+                </div>
+            </div>
+        </nav>
     )
-};
+}
 
-export default HeaderContainer;
+export default withRouter(HeaderContainer);
